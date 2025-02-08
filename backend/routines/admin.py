31b30bce216e028e5c7d90481b6f27bec849e36f
Marco Corbera <marco.corbera1@gmail.com>
2025-02-08 15:36:36 -0500

    Add admins

--- backend/routines/admin.py
+++ backend/routines/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from routines.models import Routine, Exercise
 
-# Register your models here.
+
+@admin.register(Routine)
+class RoutineAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'day', 'time', 'user')
+    list_filter = ('day', 'user')
+    search_fields = ('name', 'user__username')
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'sets', 'reps', 'routine')
+    list_filter = ('routine',)
+    search_fields = ('name', 'routine__name')
